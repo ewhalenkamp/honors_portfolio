a85f025d7a6837d772d2bd6ebdc82e47860a1d6e
Erik Halenkamp <ewhalenkamp@gmail.com>
2022-04-13 18:26:21 -0600

    initial commit

---
+++ honors_portfolio/urls.py
@@ -0,0 +1,27 @@
+"""portfolio URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from .views import *
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('evidence-of-learning/<int:subject_id>', views.eol, name='evidence-of-learning'),
+    path('honors-connect', views.hc, name='honors-connect'),
+    path('process-reflection', views.pr, name='process-reflection'),
+    path('learning-goals', views.lg, name='learning-goals'),
+]
---
+++ honors_portfolio/views.py
@@ -0,0 +1,20 @@
+from django.views.generic import ListView,DetailView,TemplateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from django.shortcuts import get_object_or_404, render, redirect
+
+def home(request):
+    return render(request, 'honors_portfolio/home.html')
+
+def eol(request, subject_id):
+    templatename = 'eol-'+str(subject_id)
+    return render(request,'honors_portfolio/'+templatename+'.html')
+
+def hc(request):
+    return render(request,'honors_portfolio/hc.html')
+
+def pr(request):
+    return render(request,'honors_portfolio/pr.html')
+
+def lg(request):
+    return render(request,'honors_portfolio/lg.html')
