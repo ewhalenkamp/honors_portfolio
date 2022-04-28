709ff71bc074c50ea472fd7ab0ba7989b2fb6fcc
Erik Halenkamp <ewhalenkamp@gmail.com>
2022-04-28 05:07:32 -0600

    debug false

--- honors_portfolio/settings.py
+++ honors_portfolio/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '*^cov*mlo_cf*0eu90tixz@#6^9e#uw8j%&neism_+7kq%&%' + '#d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['https://ewhalenkamp.pythonanywhere.com', 
                  '127.0.0.1', 'http://localhost']
